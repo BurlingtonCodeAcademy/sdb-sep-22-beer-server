a37ee0627f7ce12dfc28ae3d908906150bb929c6
Paul Niemczyk <mrpndev@gmail.com>
2022-10-25 11:58:22 -0400

    Register & Login routes set

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,12 @@
 const router = require("express").Router()
 
+let db = []
+
 // TODO: Create a login route
 
-router.get("/login", (req, res) => {
+router.post("/login", (req, res) => {
     // Object destructuring
-    let { email, password } = req.query
+    let { email, password } = req.body
     console.log(password, email)
 
     res.status(202).json({
@@ -13,5 +15,30 @@ router.get("/login", (req, res) => {
     })
 })
 // TODO: Create a register route
+/* 
+    Update the db array with a new user object each time a user registers
+
+    new user should have properties of:
+    fName, lName, email, password
+*/
+
+router.post("/register", (req, res) => {
+    let { email } = req.body
+
+    try {
+        db.push(req.body)
+        console.log(db)
+        res.status(201).json({
+            message: "User created",
+            email
+        })
+    } catch (e) {
+        console.log(e)
+        res.send(500).json({
+            message: `Error ${e}`
+        })
+    }
+})
+
 
 module.exports = router
